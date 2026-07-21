0a1aeb6770a16e05f9c28dbd135dcfe0d300333c
teeraponghf-cmd <teerapong.hf@gmail.com>
2026-07-21 08:30:58 +0700

    Add files via upload

---
+++ MRPDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MRPDashboard from "./MRPDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MRPDashboard />
+  </React.StrictMode>
+);
