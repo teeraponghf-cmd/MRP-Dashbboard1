678f93f989290b9863f6b6c8a57bdcb26c6620b6
teeraponghf-cmd <teerapong.hf@gmail.com>
2026-08-06 16:02:05 +0700

    Update MRPDashboard.jsx

--- src/MRPDashboard.jsx
+++ src/MRPDashboard.jsx
@@ -1295,7 +1295,6 @@ export default function MRPDashboard() {
   
   const [loadedFlags, setLoadedFlags] = useState({ bom: false, inventory: false, demand: false, poPending: false, git: false, actualConsumption: false, batches: false });
   const [hydrated, setHydrated] = useState(false);
-  const [hydrating, setHydrating] = useState(false);
 
 
     // ---------------------------------------------------------
